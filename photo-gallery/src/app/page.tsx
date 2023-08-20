724b4998539f3be33d75916bdaa938449d27b5e1
Ahmad Abu Bakr <138695838+Ab3467@users.noreply.github.com>
2023-08-20 15:19:24 +0500

    --force

--- photo-gallery/src/app/page.tsx
+++ photo-gallery/src/app/page.tsx
@@ -1,16 +1,41 @@
 "use client"
 import Image from 'next/image'
-import { CldUploadButton } from 'next-cloudinary'
+import { CldImage } from 'next-cloudinary';
+import { CldUploadButton } from 'next-cloudinary';
+import {useState} from "react"
+
+type UploadResult ={
+info:{
+  public_id :string
+},
+event:'success',
+}
 
 export default function Home() {
+  const [imgId,setImgId]=useState("")
   return (
     <>
-    <main className='flex min-h-screen flex-col items-center justify-center'>
-      <CldUploadButton uploadPreset="ofjk5tpc"/>
+    <main className='flex min-h-screen flex-col items-center justify-center bg-yellow-100'>
+        <CldUploadButton 
+          onUpload={(result: UploadResult)=>{
+          setImgId(result.info.public_id)
+        }}
+        uploadPreset="py5227pn"/>
 
+        {imgId && (
+        <CldImage
+          width="500"
+          height="300"
+          src={imgId}
+          sizes="100vw"
+          blurFaces
+          // tint="70:blue:green:purple"
+          alt="Description of my image"
+        />
+)}
     </main>
     
     </>
-  )
+  );
 }
     
